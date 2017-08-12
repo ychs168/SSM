d8a47199b092120d99721aeba3977906ae15e50e
英才汇硕 <30660292+ychs168@users.noreply.github.com>
2017-08-12 14:05:20 +0800

    Service层文件

--- src/service/IUserService.java
+++ src/service/IUserService.java
@@ -6,9 +6,17 @@ package com.ychs.service;
 import com.ychs.vo.Users;
 
 /**
- * @author lenovo
+ * 进行登录验证的业务接口IUserService
+ * @author wuzhijun
+ * @version 1.0
  *
  */
 public interface IUserService {
+	/**
+	 * 登录验证业务方法
+	 * @param username  用户名
+	 * @param userpsw   密码
+	 * @return  如果登录失败返回null，否则返回非空
+	 */
 	public Users login(String username,String userpsw);
 }
--- src/service/UserServiceImpl.java
+++ src/service/UserServiceImpl.java
@@ -10,16 +10,21 @@ import com.ychs.dao.IUserDao;
 import com.ychs.vo.Users;
 
 /**
- * @author lenovo
+ * 实现IUserService接口的类
+ * 
+ * @author wuzhijun
+ * @version 1.0
  *
  */
-@Service
+
+@Service  //告知spring，该类要被纳入spring的管理范围
 public class UserServiceImpl implements IUserService { 
-	@Autowired
-	private IUserDao userdao;
+	
+	@Autowired  // 这个属性的赋值由spring自动完成
+	private IUserDao userdao; 
 	
 	public Users login(String username, String userpsw) {
-		return userdao.checkUser(username, userpsw);
+		return userdao.checkUser(username, userpsw);//调用userdao对象的方法，检查用户名和密码是否正确。
 	}
- 
-}   
+
+}
