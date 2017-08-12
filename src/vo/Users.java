4321f575557db64440cc2bedc2c62226e826286f
英才汇硕 <30660292+ychs168@users.noreply.github.com>
2017-08-12 13:49:22 +0800

    Users.java

---
+++ src/vo/Users.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.ychs.vo;
+
+/**
+ * @author lenovo
+ *
+ */
+public class Users {
+	private String userid;
+	private String username;
+	private String userpsw;
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserpsw() {
+		return userpsw;
+	}
+
+	public void setUserpsw(String userpsw) {
+		this.userpsw = userpsw;
+	}
+
+}
